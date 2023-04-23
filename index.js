f077257e946bd89cc150266d56fac2551c1bf60a
Lucky Indra Efendi <90430786+LuckyIndraEfendi@users.noreply.github.com>
2023-04-23 21:36:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
-const otakudesu = require("./routes/otakudesu/otakudesu");
-const animeIndo = require("./routes/animeIndo/animeIndo");
-const kuramanime = require("./routes/kuramanime/kuramanime");
+axios.default.validateStatus = () => true;
+axios.default.headers.common["User-Agent"] =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54";
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -13,6 +13,10 @@ app.use((req, res, next) => {
   res.header("X-Powered-By", "Express");
   next();
 });
+
+const otakudesu = require("./routes/otakudesu/otakudesu");
+const animeIndo = require("./routes/animeIndo/animeIndo");
+const kuramanime = require("./routes/kuramanime/kuramanime");
 
 app.use("/otakudesu", otakudesu);
 app.use("/animeindo", animeIndo);
