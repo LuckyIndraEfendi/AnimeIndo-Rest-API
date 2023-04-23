b4b4d275ee9a872e0d6404cc1cd148ddaad660b9
Lucky Indra Efendi <90430786+LuckyIndraEfendi@users.noreply.github.com>
2023-04-23 21:38:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
-axios.default.validateStatus = () => true;
-axios.default.headers.common["User-Agent"] =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54";
-
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("X-Powered-By", "Express");
-  next();
-});
 
 const otakudesu = require("./routes/otakudesu/otakudesu");
 const animeIndo = require("./routes/animeIndo/animeIndo");
